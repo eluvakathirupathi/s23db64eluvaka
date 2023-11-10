5aa804bdef684d47dbf533df0d3da80ab052a91d
thirupathieluvaka <eluvakathirupathi@gmail.com>
2023-11-10 07:56:56 -0600

    added code to update the dog details using put

--- controllers/dog.js
+++ controllers/dog.js
@@ -23,18 +23,26 @@ exports.dog_view_all_Page = async function(req, res) {
     }
    };
 // for a specific Dog.
-exports.dog_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Dog detail: ' + req.params.id);
+exports.dog_detail = async function(req, res) {
+//  res.send('NOT IMPLEMENTED: Dog detail: ' + req.params.id);
+console.log("detail" + req.params.id)
+try {
+result = await Dog.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
 };
 // Handle Dog create on POST.
-// Handle Costume create on POST.
+// Handle Dog create on POST.
 exports.dog_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Dog();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"dog_type":"goat", "cost":12, "size":"large"}
     document.name = req.body.name;
     document.age = req.body.age;
     document.breed = req.body.breed;
@@ -52,6 +60,23 @@ exports.dog_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Dog delete DELETE ' + req.params.id);
 };
 // Handle Dog update form on PUT.
-exports.dog_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Dog update PUT' + req.params.id);
+exports.dog_update_put = async function(req, res) {
+//  res.send('NOT IMPLEMENTED: Dog update PUT' + req.params.id);
+console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await Dog.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.name) 
+    toUpdate.name = req.body.name;
+ if(req.body.cost) toUpdate.cost = req.body.cost;
+ if(req.body.size) toUpdate.size = req.body.size;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`);
+ }
 };
