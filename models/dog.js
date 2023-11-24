bf4c9685a500f48bb973d835582f66e776784b0c
thirupathieluvaka <eluvakathirupathi@gmail.com>
2023-11-23 19:32:25 -0600

    added age validator to be greater than 1 and less than 20

--- models/dog.js
+++ models/dog.js
@@ -1,6 +1,23 @@
 const mongoose=require('mongoose');
 
-const dogSchema=new mongoose.Schema({name: String,age: Number,breed: String});
+const dogSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        validate: {
+          validator: function (value) {
+            return /^[a-zA-Z]+$/.test(value);
+          },
+          message: 'Name should contain only alphabets'
+        },
+        required: [true, 'Name is required']
+      },
+      age: {
+        type: Number,
+        min: [1, 'Age must be at least 1 year'],
+        max: [20, 'Age cannot exceed 20 years'],
+        required: [true, 'Age is required']
+      },
+    breed: String});
 
 const Dog = mongoose.model('Dog', dogSchema);
 
